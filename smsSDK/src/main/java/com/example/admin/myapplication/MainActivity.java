94ce5d1b138e345375668c504b2db227f71188f9
libing <1393423989@qq.com>
2017-05-24 16:29:55 +0800

    Mob短信验证

--- smsSDK/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ smsSDK/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,13 +1,211 @@
 package com.example.admin.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
 
-    @Override
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.utils.SMSLog;
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final String DEFAULT_COUNTRY_ID = "42";
+
+    private TextView mTxtCountry;
+    private TextView mTxtCountryCode;
+    private EditText mEtxtPhone;
+    private EditText mEtxtPwd;
+    private Button mBtn;
+
+    private  SMSEvenHanlder evenHanlder;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_reg);
+        initView();
+        initData();
+        initEvent();
+
+        evenHanlder = new SMSEvenHanlder();
+        SMSSDK.registerEventHandler(evenHanlder);
+
+        //SMSSDK.getSupportedCountries();
+    }
+
+    private void initEvent() {
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCode();
+            }
+        });
+    }
+
+    private void getCode(){
+        String phone = mEtxtPhone.getText().toString().trim().replaceAll("\\s*", "");
+        String code = mTxtCountryCode.getText().toString().trim();
+        String pwd = mEtxtPwd.getText().toString().trim();
+
+        checkPhoneNum(phone,code);
+
+        //不能是86   必须是+86
+        SMSSDK.getVerificationCode(code,phone);
+    }
+
+    private void checkPhoneNum(String phone, String code) {
+        if (code.startsWith("+")) {
+            code = code.substring(1);
+        }
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "请输入手机号码",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (code == "86") {
+            if(phone.length() != 11) {
+                Toast.makeText(this, "手机号码长度不对",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        String rule = "^1(3|5|7|8|4)\\d{9}";
+        Pattern p = Pattern.compile(rule);
+        Matcher m = p.matcher(phone);
+
+        if (!m.matches()) {
+            Toast.makeText(this, "您输入的手机号码格式不正确",Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+
+    private void initData() {
+        String[] country = this.getCurrentCountry();
+        if(country != null) {
+            Log.i("========",country[1]+"="+country[0]);
+            mTxtCountryCode.setText("+"+country[1]);
+            mTxtCountry.setText(country[0]);
+        }
+    }
+
+    private void initView() {
+        mTxtCountry = (TextView) findViewById(R.id.txtCountry);
+        mTxtCountryCode = (TextView) findViewById(R.id.txtCountryCode);
+        mEtxtPhone = (EditText) findViewById(R.id.edittxt_phone);
+        mEtxtPwd = (EditText) findViewById(R.id.edittxt_pwd);
+        mBtn = (Button)findViewById(R.id.btn_next);
+    }
+
+    private String[] getCurrentCountry() {
+        String mcc = this.getMCC();
+        String[] country = null;
+        if(!TextUtils.isEmpty(mcc)) {
+            country = SMSSDK.getCountryByMCC(mcc);
+        }
+
+        if(country == null) {
+            SMSLog.getInstance().d("no country found by MCC: " + mcc, new Object[0]);
+            country = SMSSDK.getCountry("42");
+        }
+        return country;
+    }
+
+    private String getMCC() {
+        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        String networkOperator = tm.getNetworkOperator();
+        return !TextUtils.isEmpty(networkOperator)?networkOperator:tm.getSimOperator();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SMSSDK.unregisterEventHandler(evenHanlder);
+    }
+
+    /** 请求验证码后，跳转到验证码填写页面 */
+    private void afterVerificationCodeRequested(boolean smart) {
+        String phone = mEtxtPhone.getText().toString().trim().replaceAll("\\s*", "");
+        String code = mTxtCountryCode.getText().toString().trim();
+        String pwd = mEtxtPwd.getText().toString().trim();
+
+        if (code.startsWith("+")) {
+            code = code.substring(1);
+        }
+
+        Intent intent = new Intent(this,RegSecondActivity.class);
+        intent.putExtra("phone",phone);
+        intent.putExtra("pwd",pwd);
+        intent.putExtra("countryCode",code);
+
+        startActivity(intent);
+    }
+
+    private void onCountryListGot(ArrayList<HashMap<String, Object>> countries) {
+        // 解析国家列表
+        for (HashMap<String, Object> country : countries) {
+            String code = (String) country.get("zone");
+            String rule = (String) country.get("rule");
+            if (TextUtils.isEmpty(code) || TextUtils.isEmpty(rule)) {
+                continue;
+            }
+            Log.d("=========","code="+code + "rule="+rule);
+        }
+    }
+
+    class SMSEvenHanlder extends EventHandler {
+        @Override
+        public void afterEvent(final int event, final int result, final Object data) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (result == SMSSDK.RESULT_COMPLETE) {
+                        if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES) {
+                            //返回支持发送验证码的国家列表
+                            onCountryListGot((ArrayList<HashMap<String, Object>>) data);
+
+                        } else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
+                            // 请求验证码后，跳转到验证码填写页面
+                            afterVerificationCodeRequested((Boolean) data);
+
+                        } else if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+
+                        }
+                    } else {
+                        // 根据服务器返回的网络错误，给toast提示
+                        try {
+                            ((Throwable) data).printStackTrace();
+                            Throwable throwable = (Throwable) data;
+
+                            JSONObject object = new JSONObject(throwable.getMessage());
+                            String des = object.optString("detail");
+                            if (!TextUtils.isEmpty(des)) {
+                                Toast.makeText(MainActivity.this, des,Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (Exception e) {
+                            SMSLog.getInstance().w(e);
+                        }
+                    }
+                }
+            });
+        }
     }
 }
---
+++ smsSDK/src/main/java/com/example/admin/myapplication/RegSecondActivity.java
@@ -0,0 +1,152 @@
+package com.example.admin.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
+import cn.smssdk.utils.SMSLog;
+
+/**
+ * Description:
+ * Created by LiBing
+ * Data:2017/5/24 14:46
+ */
+public class RegSecondActivity extends AppCompatActivity{
+
+    private EditText mEtCode;
+    private Button mBtnResend;
+    private Button mBtnOk;
+
+    private String phone;
+    private String pwd;
+    private String countryCode;
+
+    private CountTimerView countTimerView;
+
+    private  SMSEvenHanlder evenHanlder;
+    private TextView mTxtTip;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_second);
+        initView();
+        initData();
+        initEvent();
+
+        evenHanlder = new SMSEvenHanlder();
+        SMSSDK.registerEventHandler(evenHanlder);
+
+    }
+
+    private void initEvent() {
+        mBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitCode();
+            }
+        });
+
+        mBtnResend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SMSSDK.getVerificationCode("+"+countryCode, phone);
+                countTimerView = new CountTimerView(mBtnResend,R.string.smssdk_resend_identify_code);
+                countTimerView.start();
+            }
+        });
+    }
+
+    private  void submitCode(){
+        String vCode = mEtCode.getText().toString().trim();
+
+        if (TextUtils.isEmpty(vCode)) {
+            Toast.makeText(this, R.string.smssdk_write_identify_code,Toast.LENGTH_SHORT).show();
+            return;
+        }
+        SMSSDK.submitVerificationCode(countryCode,phone,vCode);
+    }
+
+    private void initData() {
+        phone = getIntent().getStringExtra("phone");
+        pwd = getIntent().getStringExtra("pwd");
+        countryCode = getIntent().getStringExtra("countryCode");
+
+        String formatedPhone = "+" + countryCode + " " + splitPhoneNum(phone);
+
+        //String text = getString(R.string.smssdk_send_mobile_detail)+formatedPhone;
+        mTxtTip.setText("验证码已发送至"+formatedPhone);
+    }
+
+    /** 分割电话号码 */
+    private String splitPhoneNum(String phone) {
+        StringBuilder builder = new StringBuilder(phone);
+        builder.reverse();
+        for (int i = 4, len = builder.length(); i < len; i += 5) {
+            builder.insert(i, ' ');
+        }
+        builder.reverse();
+        return builder.toString();
+    }
+
+    private void initView() {
+        mEtCode = (EditText) findViewById(R.id.edittxt_code);
+        mBtnResend = (Button) findViewById(R.id.btn_reSend);
+        mBtnOk = (Button) findViewById(R.id.btn_ok);
+        mTxtTip = (TextView) findViewById(R.id.txtTip);
+        //开始计时
+        CountTimerView timerView = new CountTimerView(mBtnResend);
+        timerView.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SMSSDK.unregisterEventHandler(evenHanlder);
+    }
+
+    class SMSEvenHanlder extends EventHandler {
+
+        @Override
+        public void afterEvent(final int event, final int result, final Object data) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (result == SMSSDK.RESULT_COMPLETE) {
+                        if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                            //校验验证码，返回校验的手机和国家代码
+                            Toast.makeText(RegSecondActivity.this, "校验成功",Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        // 根据服务器返回的网络错误，给toast提示
+                        try {
+                            ((Throwable) data).printStackTrace();
+                            Throwable throwable = (Throwable) data;
+
+                            JSONObject object = new JSONObject(
+                                    throwable.getMessage());
+                            String des = object.optString("detail");
+                            if (!TextUtils.isEmpty(des)) {
+                                return;
+                            }
+                        } catch (Exception e) {
+                            SMSLog.getInstance().w(e);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
